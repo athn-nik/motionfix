56cf27d3b280ad3b1811903a465323e226495253
Nikos Athanasiou <nathanasiou@tue.mpg.de>
2023-10-20 02:44:17 +0200

    sweep maybe work ....

--- cluster/base.py
+++ cluster/base.py
@@ -8,7 +8,7 @@ import stat
 from loguru import logger
 from typing import List, Set, Dict, Tuple, Optional
 import shortuuid
-
+import wandb
 MODES = ["training", "sample_eval", "render",
          "compile_results", "relaunch_sample"]
 
@@ -37,6 +37,21 @@ SUBMISSION_TEMPLATE = f'Description=DESCRIPTION\n' \
                        '+BypassLXCfs="true"\n' \
                        'requirements=GPUS_REQS\n' \
                        'queue 1'
+
+
+
+SUBMISSION_TEMPLATE_SWEEP = f'executable = RUN_SCRIPT\n' \
+                             'arguments = $(Process) $(Cluster)\n' \
+                             'error = CNR_LOG_ID/$(Cluster).$(Process).err\n' \
+                             'output = CNR_LOG_ID/$(Cluster).$(Process).out\n' \
+                             'log = CNR_LOG_ID/$(Cluster).$(Process).log\n' \
+                             'request_memory = 128000\n' \
+                             'request_cpus=CPUS\n' \
+                             'request_gpus=NO_GPUS\n' \
+                             '+BypassLXCfs="true"\n' \
+                             'requirements=GPUS_REQS\n' \
+                             'queue QQJOBS'
+
 
 def generate_id() -> str:
     # ~3t run ids (36**4)
@@ -67,7 +82,10 @@ def launch_task_on_cluster(configs: List[Dict[str, str]],
 
     gpus_requirements = get_gpus(min_mem=gpu_min_mem, arch=gpu_arch)
     gpus_requirements = ' || '.join([f'CUDADeviceName=={x}' for x in gpus_requirements])
-    req_gpus = configs[0]['gpus']
+    if 'gpus' in configs[0]:
+        req_gpus = configs[0]['gpus']
+    else:
+        req_gpus = 1
     if req_gpus > 1:
         cpus = 6 * req_gpus
     else:
@@ -134,5 +152,69 @@ def launch_task_on_cluster(configs: List[Dict[str, str]],
             cmd = ['condor_submit_bid', f'{bid_amount}', str(submission_path)]
             logger.info('Executing ' + ' '.join(cmd))
             subprocess.run(cmd)
-    elif mode in ["sample", "relaunch_sample"]:
-        pass
+    elif mode in ["sweep"]:
+        config_file = configs[0]["config"]
+        sweep_name = configs[0]["sweep-name"]
+
+        sub_file = SUBMISSION_TEMPLATE_SWEEP
+        sub_file = sub_file.replace('EXPMODE', mode)
+        import yaml
+        pydict_sweep = yaml.safe_load(Path(config_file).read_text())
+        pydict_sweep['parameters']['logger.group']['name'] = sweep_name
+        print(pydict_sweep)
+        exit()
+        sweep_id = wandb.sweep(sweep=pydict_sweep,
+                               project="motion-editing")
+        bash = 'source /home/nathanasiou/.venvs/modit/bin/activate ' \
+               'export HYDRA_FULL_ERROR=1 export PYTHONFAULTHANDLER=1\nexport PYTHONUNBUFFERED=1\nexport PATH=$PATH\n' \
+               'export PATH=/home/nathanasiou/apps/imagemagick/bin:$PATH\n' \
+               'export LD_LIBRARY_PATH=/home/nathanasiou/apps/imagemagick/lib:$LD_LIBRARY_PATH\n' \
+               'wandb agent clockwork_pin/motion-editing/SWEEP_ID'
+
+        # cd ~/hoi-common-sense
+        # cmd=`wandb sweep --project $1 $2 2> >(grep -o "wandb agent.*$")`
+        # echo "replacing command with ${cmd}"
+        # sed -i -r "s#wandb.*#$cmd#g" cluster_run_sweep.sh
+        # cat cluster_run_sweep.sh
+        # echo ""
+        # cd
+
+        shell_dir.mkdir(parents=True, exist_ok=True)
+        run_cmd_path = shell_dir / (mode + '_' + sweep_id +".sh")
+        bash = bash.replace('SWEEP_ID', sweep_id)
+        with open(run_cmd_path, 'w') as f:
+            f.write(bash)
+        os.chmod(run_cmd_path, stat.S_IRWXU)
+
+        num_of_expers = 1
+        for k,v in pydict_sweep['parameters'].items():
+            if 'values' in v:
+                num_of_expers *= len(v['values'])
+
+        log = f'{mode}/{sweep_id}'
+        for x, y in [("NO_GPUS", str(no_gpus)), ("GPUS_REQS", gpus_requirements),
+                        ("CNR_LOG_ID", f'{CONDOR_FD}/{log}/logs'),
+                        ("CPUS", str(cpus)),
+                        ("RUN_SCRIPT", os.fspath(run_cmd_path)),
+                        ("QQJOBS", str(num_of_expers))]:
+            sub_file = sub_file.replace(x, y)
+
+        submission_path = condor_dir / log / (sweep_id + ".sub")
+        logdir_condor = condor_dir / log / 'logs'
+        logdir_condor.mkdir(parents=True, exist_ok=True)
+
+        with open(submission_path, 'w') as f:
+            f.write(sub_file)
+
+        logger.info('The cluster logs for this experiments can be found under:'\
+                    f'{str(logdir_condor)}')
+        cmd = ['condor_submit_bid', f'{bid_amount}', str(submission_path)]
+        logger.info('Executing ' + ' '.join(cmd))
+        subprocess.run(cmd)
+
+        # #!/bin/bash
+        # cd /home/mdiomataris/hoi-common-sense
+        # export PATH="/home/mdiomataris/mambaforge/envs/hoics11/bin:/home/mdiomataris/mambaforge/condabin:/lustre/home/mdiomataris/.vscode-server/bin/d045a5eda657f4d7b676dedbfa7aab8207f8a075/bin/remote-cli:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/usr/lib/jvm/java-8-oracle/bin:/usr/lib/jvm/java-8-oracle/db/bin:/usr/lib/jvm/java-8-oracle/jre/bin:/home/mdiomataris/.fzf/bin"
+        # module load cuda/11.7
+        # wandb agent markos_mpi/GoalOpt/t99kcvgj
+
