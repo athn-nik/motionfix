c84602585bdc882cf03d30379e71d314773143eb
Nikos Athanasiou <nathanasiou@tuebingen.mpg.de>
2023-12-07 04:00:09 +0100

    ...I dont' know a lot of work

--- visualize_hml3d.py
+++ visualize_hml3d.py
@@ -146,7 +146,10 @@ def render_vids(newcfg: DictConfig) -> None:
     cfg.model.diff_params.guidance_scale_text = newcfg.guidance_scale_text
 
     fd_name = get_folder_name(cfg)
+
     log_name = '__'.join(str(exp_folder).split('/')[-2:])
+    log_name = f'{log_name}_{init_diff_from}_{cfg.ckpt_name}'
+
     output_path = exp_folder / fd_name
     output_path.mkdir(exist_ok=True, parents=True)
 
