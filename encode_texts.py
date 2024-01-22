142e5dbb7399de1075a1ef763a0bcb62b6832b0f
Nikos Athanasiou <nathanasiou@tuebingen.mpg.de>
2024-01-22 07:55:07 +0100

    added new motionfix

--- encode_texts.py
+++
@@ -1,5 +0,0 @@
-_target_: src.model.textencoder.clip_encoder.ClipTextEncoder
-variant: clip_hidden
-finetune: false # if false, model weights are frozen
-last_hidden_state: true # if true, the last hidden state is used as the text embedding
-modelpath: ${path.deps}/clip-vit-large-patch14
